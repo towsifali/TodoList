5a3f071f0d166ff3361a49f7a3bef0152950bea5
Towsif <ali.towsif1@gmail.com>
2023-06-21 16:51:59 +0800

    Login Page updated

--- frontend/src/components/Auth/Login.jsx
+++ frontend/src/components/Auth/Login.jsx
@@ -16,46 +16,70 @@ export const Login = () => {
         console.log(values)
     }
   return (
-    <Flex height="100vh" align="center" justifyContent="center">
-        <Flex direction="column" alignItems="center" background={useColorModeValue('gray.100', "gray.700")} p={12} rounded={6}>
-            <Heading mb={6}>Login</Heading>
-            <form onSubmit={handleSubmit(onSubmit)}>
-                <FormControl isInvalid={errors.email}>
-                    <Input placeholder='Email' background={useColorModeValue('gray.300', "gray.600")}
-                    type="email"
-                    size="lg"
-                    mt={6}
-                    {...register("email", {required: "This is a required field"})}
-                    />
-                    <FormErrorMessage>
-                        {errors.email && errors.email.message}
-                    </FormErrorMessage>
-                </FormControl>
-                <FormControl isInvalid={errors.password}>
-                    <Input placeholder='Password' background={useColorModeValue('gray.300', "gray.600")}
-                    type="password"
-                    size="lg"
-                    mt={6}
-                    {...register("password", {required: "This is a required field"})}
-                    />
-                    <FormErrorMessage>
-                        {errors.password && errors.password.message}
-                    </FormErrorMessage>
-                </FormControl>
-                <Button 
-                isLoading ={isSubmitting}
-                loadingText="Logging in..."
-                type='submit' width="100%" colorScheme="green" variant="outline" mb={6} mt={6}>Login</Button>
-            </form>
-                <ThemeToggler showLabel={true}/>
-                <Button 
-                    onClick={() => navigate("/register", {replace : true})} 
-                    width="100%" 
-                    colorScheme="gray" 
-                    variant="outline" mt={6}>
-                        Register
-                </Button>
-        </Flex>
+    <Flex height="100vh" alignItems="center" justifyContent="center">
+      <Flex
+        direction="column"
+        alignItems="center"
+        background={useColorModeValue("gray.100", "gray.700")}
+        p={12}
+        rounded={6}
+      >
+        <Heading mb={6}>Login</Heading>
+        <form onSubmit={handleSubmit(onSubmit)}>
+          <FormControl isInvalid={errors.email}>
+            <Input
+              placeholder="Email"
+              background={useColorModeValue("gray.300", "gray.600")}
+              type="email"
+              size="lg"
+              mt={6}
+              {...register("email", {
+                required: "This is required field",
+              })}
+            />
+            <FormErrorMessage>
+              {errors.email && errors.email.message}
+            </FormErrorMessage>
+          </FormControl>
+          <FormControl isInvalid={errors.email}>
+            <Input
+              placeholder="Password"
+              background={useColorModeValue("gray.300", "gray.600")}
+              type="password"
+              size="lg"
+              mt={6}
+              {...register("password", {
+                required: "This is required field",
+              })}
+            />
+            <FormErrorMessage>
+              {errors.password && errors.password.message}
+            </FormErrorMessage>
+          </FormControl>
+          <Button
+            isLoading={isSubmitting}
+            loadingText="Logging in..."
+            width="100%"
+            colorScheme="green"
+            variant="outline"
+            mt={6}
+            mb={6}
+            type="submit"
+          >
+            Login
+          </Button>
+        </form>
+        <ThemeToggler showLabel={true} />
+        <Button
+          onClick={() => navigate("/register", { replace: true })}
+          width="100%"
+          colorScheme="gray"
+          variant="outline"
+          mt={6}
+        >
+          Register Instead
+        </Button>
+      </Flex>
     </Flex>
   );
 };

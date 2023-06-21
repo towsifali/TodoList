cfd593a5c5913b72c662a2ef274ef1c136f386d9
Towsif <ali.towsif1@gmail.com>
2023-06-21 16:29:04 +0800

    Login page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,14 @@
+import { BrowserRouter as Router, Routes, Route, Navigate} from 'react-router-dom'
+import { Login } from "./components/Auth/Login"
+
 function App() {
   return (
-   <div>Hello World</div>
+    <Router>
+      <Routes>
+        <Route path="/login" element={<Login />}/>
+        <Route path="/register" element={<h1>Register</h1>}/>
+      </Routes>
+    </Router>
   );
 }
 

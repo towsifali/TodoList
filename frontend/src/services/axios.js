423df8bfe5e028c13b1b60434249b75e3c616b52
Towsif <ali.towsif1@gmail.com>
2023-06-27 12:30:44 +0800

    Frontend Login/ Logout

---
+++ frontend/src/services/axios.js
@@ -0,0 +1,17 @@
+import { defineCssVars } from '@chakra-ui/react';
+import axios from 'axios';
+
+const baseURL = 'http://localhost:8000/api/v1/';
+
+const axiosInstance = axios.create({
+    baseURL,
+})
+
+axiosInstance.interceptors.response.user(
+    (response) => response,
+    (error) => {
+        return Promise.reject(error);
+    }
+)
+
+export default axiosInstance;
